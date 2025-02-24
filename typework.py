48483cc9585b23d37df4d0de1debd979d7b6b4a5
liyajoe <1zhouliy@hdsb.ca>
2025-02-24 14:32:07 -0500

    Variables and Print Statements Made
    All the data types were defined, and the print statements were made for the definitions and examples.

---
+++ typework.py
@@ -0,0 +1,22 @@
+#Author: Liya Zhou
+#Date Modified: February 24, 2025
+#Description: Different types of data types
+
+integer = 9
+float = 2.5
+string = "cookie"
+boolean = True
+char = 'g'
+
+integer_meaning = "a positive or negative whole number"
+float_meaning = "a number with a decimal"
+string_meaning = "a combination of characters"
+boolean_meaning = "true or false"
+char_meaning = "a single character"
+
+print(f"Here all the different types of data types in Python:\n")
+print(f"We're gonna start with an integer. An integer is {integer_meaning}, such as '{integer}'.")
+print(f"A float is {float_meaning}, like '{float}'.")
+print(f"A string is {string_meaning}, for example '{string}'.")
+print(f"A boolean means {boolean_meaning}, so '{boolean}' is a boolean!")
+print(f"Lastly, a char is {char_meaning}, such as '{char}'.")
